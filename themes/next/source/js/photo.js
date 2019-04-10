c91cce0c5b67757688f0b2253a93298e59e3ed2a
dldylan <zjpjzdl@qq.com>
2019-04-10 17:30:20 +0800

    Update posts

--- themes/next/source/js/photo.js
+++ themes/next/source/js/photo.js
@@ -19,8 +19,8 @@ photo ={
            imageSize = data[i].split(' ')[0];
            imageX = imageSize.split('.')[0];
            imageY = imageSize.split('.')[1];
-            li += '<div class="card" style="width:230px">' +
-                    '<div class="ImageInCard" style="height:'+ 230 * imageY / imageX + 'px">' +
+            li += '<div class="card" style="width:224px">' +
+                    '<div class="ImageInCard" style="height:'+ 224 * imageY / imageX + 'px">' +
                       '<a data-fancybox="gallery" href="/cloud/photos/' + imgNameWithPattern + '?raw=true" data-caption="' + imgName + '">' +
                         '<img src="/cloud/photos/' + imgNameWithPattern + '?raw=true"/>' +
                       '</a>' +
